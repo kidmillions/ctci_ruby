ac505c2de7e120be9b2328b8c8e7940b3144eb93
Chris <claymanchris@gmail.com>
2015-09-12 15:39:35 -0700

    2.1 problem complete with and without buffer

--- chapter_2.rb
+++ chapter_2.rb
@@ -1,4 +1,6 @@
 # Implementing a linked list
+# 2.1 remove duplicates from unsorted Linked list
+
 
 class Node
   def initialize (data)
@@ -51,5 +53,57 @@ class LinkedList
       0
     end
   end
+
+  def remove_duplicates!
+    vals = Hash.new(false)
+    if @head
+      cur_node = @head
+
+      while cur_node.next != nil
+        if vals[cur_node.data]
+          prev_node.next = cur_node.next
+        else
+          vals[cur_node.data] == true
+        end
+        prev_node = cur_node
+        cur_node = cur_node.next
+      end
+    end
+
+    self
+  end
+
+
+  def each
+    c = @head
+    until c == nil
+      yield c
+      c = c.next
+    end
+  end
+
+  def remove_duplicates_with_runner!
+    if @head
+      cur_node = @head
+
+      while cur_node != nil
+        runner = cur_node
+        while runner.next != nil
+          if runner.next.data == cur_node.data
+            runner.next = runner.next.next
+          else
+            runner = runner.next
+          end
+        end
+
+        cur_node = cur_node.next
+      end
+    end
+
+    self
+
+  end
+
+
 
 end
